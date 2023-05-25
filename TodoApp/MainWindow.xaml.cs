9f72f436b423ab8a969e0d3c4be23dab59e20c2e
volvo252 <roma53591@yandex.ru>
2023-05-25 09:51:02 +0300

    Добавьте файлы проекта.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel> _todoData;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoData = new BindingList<TodoModel>() { 
+                new TodoModel() {Text = "qwe"},
+                new TodoModel() {Text = "adasdw"},
+                new TodoModel() {Text = "2", IsDone = true}
+            };
+
+            dgTodoList.ItemsSource = _todoData;
+        }
+    }
+}
---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    internal class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;  // при создании объекта дататайм будет сразу же присваиваться текущее время
+
+		private bool _IsDone;
+		private string _text;
+
+		public bool IsDone
+		{
+			get { return _IsDone; }
+			set { _IsDone = value; }
+		}
+		public string Text
+		{
+			get { return _text; }
+			set { _text = value; }
+		}
+
+	}
+}
